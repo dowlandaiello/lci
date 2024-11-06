01029e9e581deef5ac38fca327d7fca41ff56696
Dowland Aiello <dowlandaiello@gmail.com>
2024-11-06 05:57:17 +0000

    More reorganization: moving entrypoints to cmd file.

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,3 +1,60 @@
+use super::MAXIMUM_EVALUATION_CYCLES;
+use lclib::{
+    parser::{self, Expr},
+    reducer::{NaiveReducer, Reducer},
+};
+use std::{
+    fs::OpenOptions,
+    io::{self, Read, Write},
+};
+
+macro_rules! read_eof {
+    ($in:ident, $out:ident, $prompt:expr, $t:ty) => {{
+        print!("{}", $prompt);
+
+        $out.flush().expect("failed to flush input");
+
+        let mut input_buff = String::new();
+        let n_bytes = $in
+            .read_line(&mut input_buff)
+            .expect("failed to read input");
+
+        if n_bytes == 0 {
+            println!("");
+
+            break;
+        }
+
+        input_buff.trim().parse::<$t>()
+    }};
+}
+
+pub fn exec_script(fname: &str) {
+    let mut f = OpenOptions::new().read(true).open(fname).unwrap();
+
+    let mut buff = String::new();
+    f.read_to_string(&mut buff)
+        .expect("failed to read expression file");
+
+    let tok_stream = parser::lex(&buff).expect("failed to lex");
+    let mut expr: Expr = tok_stream
+        .as_slice()
+        .try_into()
+        .expect("failed to construct AST");
+
+    for _ in 0..MAXIMUM_EVALUATION_CYCLES {
+        let new_res = NaiveReducer::step(expr.clone());
+
+        if new_res == expr {
+            break;
+        }
+
+        expr = new_res;
+    }
+
+    println!("{}", expr);
+}
+
 pub fn repl() {
     let input = io::stdin();
     let mut output = io::stdout();
